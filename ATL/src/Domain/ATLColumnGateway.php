ad6d8e10793109649a635ca0425d6afb85e5a36f
Ray Clark <raynichc@users.noreply.github.com>
2021-09-14 11:33:16 +1000

    Added ATLColumnGateway

---
+++ ATL/src/Domain/ATLColumnGateway.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Gibbon\Module\ATL\Domain;
+
+use Gibbon\Domain\Traits\TableAware;
+use Gibbon\Domain\QueryCriteria;
+use Gibbon\Domain\QueryableGateway;
+
+/**
+ * Trip Log Gateway
+ */
+class ATLColumnGateway extends QueryableGateway
+{
+    use TableAware;
+
+    private static $tableName = 'atlColumn';
+    private static $primaryKey = 'atlColumnID';
+    private static $searchableColumns = [];
+
+    /**
+     * @param QueryCriteria $criteria
+     * @return DataSet
+     */
+    public function queryATLColumnsByClass(QueryCriteria $criteria, $gibbonCourseClassID)
+    {
+        $query = $this
+            ->newQuery()
+            ->from($this->getTableName())
+            ->cols([
+                'atlColumnID', 'gibbonCourseClassID', 'groupingID', 'name', 'description', 'gibbonRubricID', 'complete', 'completeDate', 'gibbonPersonIDCreator', 'gibbonPersonIDLastEdit'
+            ])
+            ->where('atlColumn.gibbonCourseClassID = :gibbonCourseClassID')
+            ->bindValue('gibbonCourseClassID', $gibbonCourseClassID);
+
+        return $this->runQuery($query, $criteria);
+    }
+    
+    /**
+     * @param QueryCriteria $criteria
+     * @return DataSet
+     */
+    public function queryATLColumnsByStudent(QueryCriteria $criteria, $gibbonSchoolYearID, $gibbonPersonID)
+    {
+        $query = $this
+            ->newQuery()
+            ->from($this->getTableName())
+            ->cols([
+              'atlColumn.name' 
+            ])
+            ->leftJoin('atlEntry', 'atlColumn.atlColumnID=atlEntry.atlColumnID AND atlEntry.gibbonPersonIDStudent = :gibbonPersonID')
+            ->leftJoin('gibbonCourseClass', 'atlColumn.gibbonCourseClassID = gibbonCourseClass.gibbonCourseClassID')
+            ->leftJoin('gibbonCourseClassPerson', 'gibbonCourseClass.gibbonCourseClassID = gibbonCourseClassPerson.gibbonCourseClassID')
+            ->leftJoin('gibbonCourse', 'gibbonCourseClass.gibbonCourseID = gibbonCourse.gibbonCourseID')
+            ->where('gibbonCourseClassPerson.gibbonPersonID = :gibbonPersonID')
+            ->where("gibbonCourseClassPerson.role = 'Student'")
+            ->where('gibbonCourse.gibbonSchoolYearID = :gibbonSchoolYearID')
+            ->bindValue('gibbonPersonID', $gibbonPersonID)
+            ->bindValue('gibbonSchoolYearID', $gibbonSchoolYearID);
+
+        return $this->runQuery($query, $criteria);
+    }
+}
