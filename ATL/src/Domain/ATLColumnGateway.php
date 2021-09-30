34ef24d48dfe64f550f0cd4483e0bd74889a85e1
Ray Clark <raynichc@users.noreply.github.com>
2021-10-01 08:36:28 +1000

    Added complete flag and isForStudents function

--- ATL/src/Domain/ATLColumnGateway.php
+++ ATL/src/Domain/ATLColumnGateway.php
@@ -9,8 +9,8 @@ use Gibbon\Domain\QueryableGateway;
 /**
  * ATL Column Gateway
  *
- * @version v20
- * @since   v20
+ * @version v22
+ * @since   v22
  */
 class ATLColumnGateway extends QueryableGateway
 {
@@ -42,25 +42,53 @@ class ATLColumnGateway extends QueryableGateway
      * @param QueryCriteria $criteria
      * @return DataSet
      */
-    public function queryATLColumnsByStudent(QueryCriteria $criteria, $gibbonSchoolYearID, $gibbonPersonID)
+    public function queryATLColumnsByStudent(QueryCriteria $criteria, $gibbonSchoolYearID, $gibbonPersonID, $complete = '')
     {
         $query = $this
             ->newQuery()
             ->from($this->getTableName())
             ->cols([
-              'atlColumn.name' 
+              'atlColumn.atlColumnID', 'atlColumn.gibbonRubricID', 'atlColumn.gibbonCourseClassID', 'atlEntry.atlEntryID', 'atlColumn.name', 'atlColumn.description', 'atlColumn.complete', 'atlColumn.completeDate', 'gibbonCourse.nameShort AS course', 'gibbonCourseClass.nameShort AS class', 'atlEntry.complete as entryComplete'
             ])
             ->leftJoin('atlEntry', 'atlColumn.atlColumnID=atlEntry.atlColumnID AND atlEntry.gibbonPersonIDStudent = :gibbonPersonID')
             ->leftJoin('gibbonCourseClass', 'atlColumn.gibbonCourseClassID = gibbonCourseClass.gibbonCourseClassID')
             ->leftJoin('gibbonCourseClassPerson', 'gibbonCourseClass.gibbonCourseClassID = gibbonCourseClassPerson.gibbonCourseClassID')
             ->leftJoin('gibbonCourse', 'gibbonCourseClass.gibbonCourseID = gibbonCourse.gibbonCourseID')
+            ->where("atlColumn.forStudents = 'Y'")
             ->where('gibbonCourseClassPerson.gibbonPersonID = :gibbonPersonID')
             ->where("gibbonCourseClassPerson.role = 'Student'")
             ->where('gibbonCourse.gibbonSchoolYearID = :gibbonSchoolYearID')
             ->bindValue('gibbonPersonID', $gibbonPersonID)
             ->bindValue('gibbonSchoolYearID', $gibbonSchoolYearID);
 
+        if (!empty($complete)) {
+            $query->where('atlEntry.complete = :complete')
+                ->bindValue('complete', $complete);
+        }
+
         return $this->runQuery($query, $criteria);
     }
     
+    public function isForStudent($atlColumnID, $gibbonPersonID, $gibbonSchoolYearID) {
+        $select = $this
+            ->newSelect()
+            ->from($this->getTableName())
+            ->cols([
+              'atlColumn.atlColumnID'
+            ])
+            ->leftJoin('gibbonCourseClass', 'atlColumn.gibbonCourseClassID = gibbonCourseClass.gibbonCourseClassID')
+            ->leftJoin('gibbonCourseClassPerson', 'gibbonCourseClass.gibbonCourseClassID = gibbonCourseClassPerson.gibbonCourseClassID')
+            ->leftJoin('gibbonCourse', 'gibbonCourseClass.gibbonCourseID = gibbonCourse.gibbonCourseID')
+            ->where('atlColumn.atlColumnID = :atlColumnID')
+            ->where("atlColumn.forStudents = 'Y'")
+            ->where('gibbonCourseClassPerson.gibbonPersonID = :gibbonPersonID')
+            ->where("gibbonCourseClassPerson.role = 'Student'")
+            ->where('gibbonCourse.gibbonSchoolYearID = :gibbonSchoolYearID')
+            ->bindValue('atlColumnID', $atlColumnID)
+            ->bindValue('gibbonPersonID', $gibbonPersonID)
+            ->bindValue('gibbonSchoolYearID', $gibbonSchoolYearID);
+
+        $result = $this->runSelect($select);
+        return $result->isNotEmpty();
+    }
 }
